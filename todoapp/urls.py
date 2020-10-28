0ba68509dd899294b42faeacd1306ea716dced4f
DishenMakwana <58837408+DishenMakwana@users.noreply.github.com>
2020-10-28 11:14:18 +0530

    version 1.0

---
+++ todoapp/urls.py
@@ -0,0 +1,10 @@
+from todolist.views import index
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name="TodoList"),
+    path('todolist/',include('todolist.urls')),
+]
